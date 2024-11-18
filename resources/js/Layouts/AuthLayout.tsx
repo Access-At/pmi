d4ee348faf230e888eef46207ca5d2a4466c0fb0
Farhan Reizha <48412599+farhanreizha@users.noreply.github.com>
2024-11-19 00:38:57 +0700

    fix: responsive

--- resources/js/Layouts/AuthLayout.tsx
+++ resources/js/Layouts/AuthLayout.tsx
@@ -9,19 +9,19 @@ interface Props {
 
 export default function AuthLayout({ children, title }: Props) {
     return (
-        <GuestLayout>
+        <div className="flex flex-col max-h-screen sm:flex-row">
             <Head title={title} />
 
-            <div className="flex w-full bg-white">
+            <div className="flex w-full h-screen bg-white flex-col md:flex-row">
                 {children}
-                <section className="">
+                <section className="hidden md:block">
                     <img
                         src="assets/images/background.png"
                         alt="images"
-                        className="w-full h-screen"
+                        className="w-full h-screen object-cover"
                     />
                 </section>
             </div>
-        </GuestLayout>
+        </div>
     );
 }
--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,10 +1,13 @@
-import { Link } from "@inertiajs/react";
+import Footer from "@/Components/Footer";
+import Navbar from "@/Components/Navbar";
 import { PropsWithChildren } from "react";
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="flex max-h-screen items-center">
+        <div className="flex flex-col max-h-screen">
+            <Navbar />
             <main className="h-screen w-full">{children}</main>
+            <Footer />
         </div>
     );
 }
