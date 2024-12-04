3344373320aed0cf9316ceca134bfb363a55822f
Farhan Reizha <48412599+farhanreizha@users.noreply.github.com>
2024-12-04 17:53:44 +0700

    feat: component time-input

--- resources/js/Components/section/dashboard/DashboardTable.tsx
+++ resources/js/Components/section/dashboard/DashboardTable.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/Components/ui/button";
+import { ScrollArea } from "@/Components/ui/scroll-area";
 import {
     Table,
     TableBody,
@@ -17,42 +18,46 @@ export default function DashboardTable({
     head: any;
 }) {
     return (
-        <Table>
-            <TableHeader className="border-y">
-                <TableRow>
+        <ScrollArea className="h-[30rem] w-full">
+            <Table>
+                <TableHeader className="border-y">
+                    <TableRow>
+                        <EachUtil
+                            of={head}
+                            render={(head) => (
+                                <TableHead>{head.name}</TableHead>
+                            )}
+                        />
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
                     <EachUtil
-                        of={head}
-                        render={(head) => <TableHead>{head.name}</TableHead>}
+                        of={data}
+                        render={(event) => (
+                            <TableRow key={event.id}>
+                                <TableCell>{event.name}</TableCell>
+                                <TableCell>{event.datetime}</TableCell>
+                                <TableCell>{event.location}</TableCell>
+                                <TableCell>{event.description}</TableCell>
+                                <TableCell className="text-right space-x-2">
+                                    <Button
+                                        size="sm"
+                                        className="bg-cyan-300/50 hover:bg-cyan-300 text-cyan-600 hover:text-white"
+                                    >
+                                        Update
+                                    </Button>
+                                    <Button
+                                        size="sm"
+                                        className="bg-primary/30 hover:bg-primary text-red-600 hover:text-white"
+                                    >
+                                        Delete
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        )}
                     />
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                <EachUtil
-                    of={data}
-                    render={(event) => (
-                        <TableRow key={event.id}>
-                            <TableCell>{event.name}</TableCell>
-                            <TableCell>{event.datetime}</TableCell>
-                            <TableCell>{event.location}</TableCell>
-                            <TableCell>{event.description}</TableCell>
-                            <TableCell className="text-right space-x-2">
-                                <Button
-                                    size="sm"
-                                    className="bg-cyan-300/50 hover:bg-cyan-300 text-cyan-600 hover:text-white"
-                                >
-                                    Update
-                                </Button>
-                                <Button
-                                    size="sm"
-                                    className="bg-primary/30 hover:bg-primary text-red-600 hover:text-white"
-                                >
-                                    Delete
-                                </Button>
-                            </TableCell>
-                        </TableRow>
-                    )}
-                />
-            </TableBody>
-        </Table>
+                </TableBody>
+            </Table>
+        </ScrollArea>
     );
 }
